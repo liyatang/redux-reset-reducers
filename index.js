cc015100c01858444bfaff73a6947e95513dd5d1
liyatang <liyatang@xiaonongnv.com>
2016-12-16 12:14:41 +0800

    c

---
+++ index.js
@@ -0,0 +1,13 @@
+export default function reduxResetReducers(reducers, resetType = 'RESET_STATE'){
+    const newReducers = {};
+
+    Object.keys(reducers).forEach(function(key){
+        const reducer = reducers[key];
+        newReducers[key] = (state, action) => {
+            if (action.type === resetType) {
+                return reducer(undefined, {});
+            }
+            return reducer(state, action);
+        }
+    });
+}
